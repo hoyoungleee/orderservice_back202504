63e298e9e802fb8149f5aa30bd8ee6e8721a4bed
hoyoungleee <ghdud939@gmail.com>
2025-04-22 09:35:08 +0900

    로그인, 주문, 재고관리 프로젝트 첫 커밋

---
+++ src/main/java/com/playdata/orderservice/OrderserviceBack202504Application.java
@@ -0,0 +1,13 @@
+package com.playdata.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceBack202504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderserviceBack202504Application.class, args);
+    }
+
+}
